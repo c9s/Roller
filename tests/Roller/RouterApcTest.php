e9dd0d789bd32a4212e318af67ce657bf92e7b85
c9s <cornelius.howl@gmail.com>
2012-02-29 14:05:27 +0800

    separate apc router tests for travis

---
+++ tests/Roller/RouterApcTest.php
@@ -0,0 +1,44 @@
+<?php
+
+
+class RouterApcTest extends PHPUnit_Framework_TestCase
+{
+    function setup()
+    {
+        if (!extension_loaded('apc')) {
+            $this->markTestSkipped( 'The apc extension is not available.' );
+        }
+    }
+
+	function testApcCache()
+	{
+		ini_set('apc.enable_cli',1);
+
+		apc_store( 'test', 3 );
+		is( 3, apc_fetch( 'test' ) );
+
+		$router = new Roller\Router( null , array( 
+			'cache_id' => '_router_testing_'
+	   	));
+		is( Roller\Router::cache_type_apc , $router->cache );
+
+		$router->add('/item/:id', function($id) { return $id; });
+		$r = $router->dispatch( '/item/12' );
+
+		$code = apc_fetch( '_router_testing_' );
+		ok( $code );
+		$routes = eval($code);
+		ok( $routes );
+		ok( $routes->routes );
+
+
+
+		// make sure cache reload works
+		$router = new Roller\Router( null , array( 
+			'cache_id' => '_router_testing_'
+	   	));
+		$r = $router->dispatch( '/item/12' );
+		is( '12' , $r() );
+		apc_delete( '_router_testing_' );
+	}
+}
--- tests/Roller/RouterTest.php
+++ tests/Roller/RouterTest.php
@@ -75,39 +75,6 @@ class RouterTest extends PHPUnit_Framework_TestCase
         ok( $r === false );
     }
 
-	function testApcCache()
-	{
-		ini_set('apc.enable_cli',1);
-
-		apc_store( 'test', 3 );
-		is( 3, apc_fetch( 'test' ) );
-
-		$router = new Roller\Router( null , array( 
-			'cache_id' => '_router_testing_'
-	   	));
-		is( Roller\Router::cache_type_apc , $router->cache );
-
-		$router->add('/item/:id', function($id) { return $id; });
-		$r = $router->dispatch( '/item/12' );
-
-		$code = apc_fetch( '_router_testing_' );
-		ok( $code );
-		$routes = eval($code);
-		ok( $routes );
-		ok( $routes->routes );
-
-
-
-		// make sure cache reload works
-		$router = new Roller\Router( null , array( 
-			'cache_id' => '_router_testing_'
-	   	));
-		$r = $router->dispatch( '/item/12' );
-		is( '12' , $r() );
-		apc_delete( '_router_testing_' );
-	}
-
-
 	function testFileCache()
 	{
 		if( ! file_exists('tests/cache') )
