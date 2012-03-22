985dad21fee77d779f6903f444602bc665d5ff80
c9s <cornelius.howl@gmail.com>
2012-03-22 17:14:28 +0800

    Reindent, skip add or mount method if we have a cache already.

--- src/Roller/Router.php
+++ src/Roller/Router.php
@@ -4,17 +4,17 @@ use Roller\Exception\RouterException;
 
 class Router
 {
-	public $routes;
+    public $routes;
 
 
-	/* boolean, is cache enabled ? */
-	public $cache;
+    /* boolean, is cache enabled ? */
+    public $cache;
 
 
     /**
      * is cache found ?
      */
-	public $hasCache = false;
+    public $hasCache = false;
 
 
     /**
@@ -22,7 +22,7 @@ class Router
      *
      * @var string path
      */
-	public $cacheDir;
+    public $cacheDir;
 
 
     public $matchedRouteClass = 'Roller\MatchedRoute';
@@ -33,7 +33,7 @@ class Router
      *
      * @var string cache if for apc and file.
      */
-	public $cacheId;
+    public $cacheId;
 
 
     public $enableExtension = true;
@@ -47,7 +47,7 @@ class Router
      * should we reload cache ? (not implemented yet)
      *
      */
-	public $reload = false;
+    public $reload = false;
 
 
     /**
@@ -56,12 +56,12 @@ class Router
     public $plugins = array();
 
 
-	const cache_type_apc = 1;
+    const cache_type_apc = 1;
 
-	const cache_type_file = 2;
+    const cache_type_file = 2;
 
-	public function __construct($routes = null, $options = array() )
-	{
+    public function __construct($routes = null, $options = array() )
+    {
         /* setup custom route class */
         if( isset($options['route_class']) ) {
             $this->matchedRouteClass = $options['route_class'];
@@ -71,9 +71,9 @@ class Router
             throw new RouterException('Router constructor argument #1 is not a Roller\RouteSet object.');
         }
 
-		/* if cache_id is defined (only), we use apc for caching defined routes */
-		if( isset($options['cache_id']) ) {
-			$this->cacheId = $options['cache_id'];
+        /* if cache_id is defined (only), we use apc for caching defined routes */
+        if( isset($options['cache_id']) ) {
+            $this->cacheId = $options['cache_id'];
 
             if( extension_loaded('apc') ) {
                 $this->cache = self::cache_type_apc;
@@ -82,14 +82,14 @@ class Router
                     $this->hasCache = true;
                 }
             }
-		}
+        }
 
-		if( isset($options['cache_dir']) ) {
-			$this->cacheDir = $options['cache_dir'];
-			$this->cache = self::cache_type_file;
-			$cacheFile = $this->cacheDir . DIRECTORY_SEPARATOR . $this->cacheId;
-			$this->hasCache = false;
-			if( file_exists($cacheFile) ) {
+        if( isset($options['cache_dir']) ) {
+            $this->cacheDir = $options['cache_dir'];
+            $this->cache = self::cache_type_file;
+            $cacheFile = $this->cacheDir . DIRECTORY_SEPARATOR . $this->cacheId;
+            $this->hasCache = false;
+            if( file_exists($cacheFile) ) {
                 // check expiry 
                 if( $this->cacheExpiry 
                     && ( filemtime($cacheFile) + $this->cacheExpiry ) < time() ) 
@@ -100,25 +100,29 @@ class Router
                     $this->routes = require $cacheFile;
                     $this->hasCache = true;
                 }
-			}
-		}
+            }
+        }
 
-		if( isset($options['reload']) ) {
-			$this->reload = $options['reload'];
-		}
+        if( isset($options['reload']) ) {
+            $this->reload = $options['reload'];
+        }
 
-		if( null == $this->routes ) {
-			$this->routes = $routes ?: new RouteSet;
-		}
-	}
+        if( null == $this->routes ) {
+            $this->routes = $routes ?: new RouteSet;
+        }
+    }
 
-	public function add($path,$callback,$options=array() )
-	{
-		return $this->routes->add( $path, $callback, $options );
-	}
+    public function add($path,$callback,$options=array() )
+    {
+        if( $this->hasCache )
+            return;
+        return $this->routes->add( $path, $callback, $options );
+    }
 
     public function mount($prefix,$routeset) 
     {
+        if( $this->hasCache )
+            return;
         return $this->routes->mount( $prefix , $routeset );
     }
 
@@ -128,47 +132,47 @@ class Router
         $this->plugins[] = $plugin;
     }
 
-	private function makeCache()
-	{
-		if( $this->cache === self::cache_type_apc ) {
-			$dumper = new \Roller\Dumper\PhpDumper;
-			$code = $dumper->dump( $this->routes );
-			apc_store( $this->cacheId, $code ) === true 
-				or die('Roller\Router: apc cache failed.');
-		}
-		elseif( $this->cache === self::cache_type_file ) {
-			$dumper = new \Roller\Dumper\PhpDumper;
-			$code = $dumper->dump( $this->routes );
-			$cacheFile = $this->cacheDir . DIRECTORY_SEPARATOR . $this->cacheId;
-			file_put_contents( $cacheFile , '<?php ' . $code ) !== false
-				or die('Roller\Router: file cache failed.');
-		}
-		else {
-			throw new Exception('Unknown cache type');
-		}
-	}
+    private function makeCache()
+    {
+        if( $this->cache === self::cache_type_apc ) {
+            $dumper = new \Roller\Dumper\PhpDumper;
+            $code = $dumper->dump( $this->routes );
+            apc_store( $this->cacheId, $code ) === true 
+                or die('Roller\Router: apc cache failed.');
+        }
+        elseif( $this->cache === self::cache_type_file ) {
+            $dumper = new \Roller\Dumper\PhpDumper;
+            $code = $dumper->dump( $this->routes );
+            $cacheFile = $this->cacheDir . DIRECTORY_SEPARATOR . $this->cacheId;
+            file_put_contents( $cacheFile , '<?php ' . $code ) !== false
+                or die('Roller\Router: file cache failed.');
+        }
+        else {
+            throw new Exception('Unknown cache type');
+        }
+    }
 
-	public function dispatch($path)
-	{
-		if( ! $this->hasCache ) {
+    public function dispatch($path)
+    {
+        if( ! $this->hasCache ) {
 
             foreach( $this->plugins as $p )
                 $p->beforeCompile( $this );
 
-			$this->routes->compile();
+            $this->routes->compile();
 
             foreach( $this->plugins as $p )
                 $p->afterCompile( $this );
 
-			if( $this->cache ) {
-				// make cache
-				$this->makeCache();
-			}
+            if( $this->cache ) {
+                // make cache
+                $this->makeCache();
+            }
 
-			// we are already in runtime, doesn't need to reload cache or 
-			// re-compile pattern.
-			$this->hasCache = true;
-		}
+            // we are already in runtime, doesn't need to reload cache or 
+            // re-compile pattern.
+            $this->hasCache = true;
+        }
 
         if( $this->enableExtension && function_exists('roller_dispatch') ) {
             $route = roller_dispatch( $this->routes->routes , $path );
@@ -188,9 +192,9 @@ class Router
                     // if method is defined, we should check server request method
                     if( $server_req_method && isset($route['method']) && $m = $route['method'] ) {
                         /* 
-                        * Which request method was used to access the page; 
-                        * i.e. 'GET', 'HEAD', 'POST', 'PUT'.
-                        */
+                         * Which request method was used to access the page; 
+                         * i.e. 'GET', 'HEAD', 'POST', 'PUT'.
+                         */
                         if( $server_req_method !== $m )
                             continue;
                     }
@@ -211,8 +215,8 @@ class Router
                 }
             }
         }
-		return false;
-	}
+        return false;
+    }
 
     /**
      * dispatch methods to plugins (mixin) and routeset methods
