41dc528db60d75165e07702494729eb8d94a5727
c9s <cornelius.howl@gmail.com>
2012-04-09 03:31:46 +0800

    Fix optional field compile

--- tests/Roller/RouteCompilerTest.php
+++ tests/Roller/RouteCompilerTest.php
@@ -6,18 +6,19 @@ class RouteCompilerTest extends PHPUnit_Framework_TestCase
     function test()
     {
         $route = Roller\RouteCompiler::compile(array( 
-            'path' => '/blog/:id(\.:format)',
+            'path' => '/blog/:id(.:format)',
             'default' => array(
                 'format' => 'json'
             )
         ));
+        # echo $route['compiled'];
         ok( preg_match( $route['compiled'] , '/blog/23.json', $matched ) );
 
         is( '23', $matched['id'] );
         is( 'json', $matched['format'] );
 
         ok( preg_match( $route['compiled'] , 
-            '/blog/23/like/json', $matched ) );
+            '/blog/24.json', $matched ) );
         is( 'json', $matched['format'] );
 
         ok( ! preg_match( $route['compiled'] , 
