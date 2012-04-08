9e6d52297768e932f093bbee9cfbc7b13110caf0
c9s <cornelius.howl@gmail.com>
2012-04-09 03:51:13 +0800

    Remove debug

--- demo/index.php
+++ demo/index.php
@@ -51,7 +51,6 @@ $r = $router->dispatch( isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '
 #  var_dump( $_SERVER['PATH_INFO'] ); 
 #  var_dump( $r['compiled'] ); 
 #  var_dump( $r['vars'] ); 
-var_dump( $r ); 
 
 if( $r )
     echo $r();
