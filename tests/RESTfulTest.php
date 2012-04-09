0861479701b74145ae4a044d664440f1debb4724
c9s <cornelius.howl@gmail.com>
2012-04-09 17:09:49 +0800

    Fix tests

--- tests/RESTfulTest.php
+++ tests/RESTfulTest.php
@@ -70,11 +70,11 @@ class RESTfulTest extends PHPUnit_Framework_TestCase
 
         $_SERVER['REQUEST_METHOD'] = 'GET';
         $r = $router->dispatch( '/restful/blog' );
-        is( '{"success":true,"data":[1,2,3,4,5,6,7,8,9,10],"message":"Record find success."}' , $r() );
+        is( '[1,2,3,4,5,6,7,8,9,10]' , $r() );
 
         $_SERVER['REQUEST_METHOD'] = 'GET';
         $r = $router->dispatch( '/restful/blog/3' );
-        is( '{"success":true,"data":{"id":"3"},"message":"Record 3 loaded."}' , $r() );
+        is( '{"id":"3"}' , $r() );
 
     }
 
@@ -93,11 +93,11 @@ class RESTfulTest extends PHPUnit_Framework_TestCase
 
         $_SERVER['REQUEST_METHOD'] = 'GET';
         $r = $router->dispatch('/restful/blog/1');
-        is( '{"success":true,"data":{"id":"1","title":"title"},"message":"Record 1 loaded."}' , $r() );
+        is( '{"id":"1","title":"title"}' , $r() );
 
         $_SRVER['REQUEST_METHOD'] = 'GET';
         $r = $router->dispatch('/restful/blog');
-        is( '{"success":true,"data":[{"id":0},{"id":1},{"id":2}],"message":"Record find success."}' , $r() );
+        is( '[{"id":0},{"id":1},{"id":2}]' , $r() );
 
         $_SRVER['REQUEST_METHOD'] = 'PUT';
         $r = $router->dispatch('/restful/blog/1');
